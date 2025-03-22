47ea3ffe7f79b5464c65f1fc7ccf16c24ae3193f
| Felipe Rafael Rothbarth | <108741421+rothibarth@users.noreply.github.com>
2025-03-21 23:05:29 -0300

    finalizado

--- ead-5-felipe-codigo_atualizado/js/scriptExtra.js
+++
@@ -1,6 +0,0 @@
-// Código duplicado sem necessidade
-function exibirUsuarios() {
-    for (var i = 0; i < usuariosArray.length; i++) {
-        console.log("Usuário: " + usuariosArray[i].nome + " - Email: " + usuariosArray[i].email);
-    }
-}
